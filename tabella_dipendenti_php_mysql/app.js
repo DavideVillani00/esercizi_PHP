337803e26583c0e08c771c2afb8da8362c3e3fae
DavideVillani00 <villani.dado@gmail.com>
2024-10-09 13:34:48 +0200

    iniziato a fare html e css e js

---
+++ tabella_dipendenti_php_mysql/app.js
@@ -0,0 +1,5 @@
+const btn_agg = document.querySelector("#aggiungi");
+const cont_agg = document.querySelector("#container_aggiungi");
+btn_agg.addEventListener("click", (e) => {
+  e.preventDefault();
+});
