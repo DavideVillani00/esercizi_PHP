ccf324256fa00b66e18caa7d2159f9fb5cc42f60
DavideVillani00 <villani.dado@gmail.com>
2024-10-26 00:31:57 +0200

    inserito cookie

--- nozioni_php_mysql/nozioni_PDO_php_mysql.php
+++ nozioni_php_mysql/nozioni_PDO_php_mysql.php
@@ -61,8 +61,9 @@ $stmt = $conn->prepare($sql);
 $stmt->execute();
 //* PRENDI I RISULTATI
 <!-- salvi il risultato in un array associativo chiamato $utenti -->
-$utenti = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//* FAI QUELLO CHE DEVI, ES STAMPA IL RISULTATO
+$utenti = $stmt->fetchAll(PDO::FETCH_ASSOC);                                    //*SE C'è SOLO UN RISULTATO PUOI USARE SOLO FETCH 
+                                                                                / $risultato = $stmt->fetch(PDO::FETCH_ASSOC);
+//* FAI QUELLO CHE DEVI, ES STAMPA IL RISULTATO                                            
 foreach ($utenti as $utente){
 <!-- utilizza htmlspecialchars() per poter evitare che il browser esegua codice html o js in caso qualcuno l'abbia inserito nel database -->
 <!-- htmlspecialchars($variabile, ENT_QUOTES); il secondo parametro serve per codificare anche le virgolette signole e doppie -->
@@ -116,6 +117,40 @@ if(password_verify($_POST["password"], <!-- QUI VA LA password nel database -->)
 }else{
     echo "password errata";
 }
+
+//? COME LAVORARE CON LE SESSIONI
+//*APRI UNA SESSIONE OGNI VOLTA CHE UTILIZZI UN QUALCOSA INERENTE
+session_start();
+$_SESSION["nome"] = "Mario"; //* PER IMPOSTARE UNA VARIABILE IN SESSIONE <!-- $_SESSION["nome variabile"] = "valore" -->
+var_dump($_SESSION["nome"]); //* per richiamare una variabile in sessione
+//! cancellare varibili e sessioni
+session_unset();
+session_destroy();
+
+//? COME USARE I COOKIE ES RESTA CONNESSO
+//* se si flegga l'input per rimanere connesso
+//! CREA E SALVA UN TOKEN NEL DATABASE DELL'UTENTE LOGGATO - CREA UN COOKIE CON IL TOKEN
+$token = bin2hex(random_bytes(32));
+                        //* nome colonna
+$sql = "UPDATE utenti SET token_ricordami = :token WHERE id = :id";
+$stmt = $conn->prepare($sql);
+$stmt->execute([":token"=>$token, ":id=>$id <!-- preso in precedenza -->"]);
+                                //*30 giorni di validità // "/" indica che è valido su tutte le pagine
+setcookie("ricordami", $token, time()+60*60*24*30, "/");
+//! CONTROLLA SU OGNI PAGINA: SE NON C'è LA SESSIONE MA C'è IL COOKIE, PRENDI IL TOKEN DEL COOKIE E CONFRONTALO CON QUELLO NEL DATABASE, SE CORRISPONDE IMPOSTA L'ID NELLA SESSIONE
+if(!isset($_SESSION["id"]) && isset($_COOKIE["token"])){
+    $sql = "SELECT * FROM utenti WHERE token_ricordami = :token";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([":token"=>$_COOKIE["token"]]);
+    $risultato = $stmt->fetch(PDO::FETCH_ASSOC);
+    $_SESSION["id"] = intval($risultato["id"]);
+}
+//! se non esiste una sessione vai al login
+if(!isset($_SESSION["id"])){
+    header("Location: <!-- percorso file login -->");
+    exit();
+}
+
 
 
 }catch(PDOException $e){
